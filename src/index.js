44b16c29e8da16f3d0677c8c94fce794f278c603
Darking053 <darking053@protonmail.com>
2026-04-25 18:31:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-const { Client } = require('./system');
+const { Client } = require('./client');
 const { VoiceClient } = require('./voice');
 const { WebhookClient } = require('./webhook');
 const { RESTClient } = require('./rest');
+const { NetworkManager } = require('./network');
 const { version, colors, badges } = require('./types');
-const { sleep, formatTime, randomString, escapeHtml } = require('./utils');
+const { sleep, formatTime, formatDate, randomString, escapeHtml, truncate } = require('./utils');
 
 module.exports = {
     Client,
     VoiceClient,
     WebhookClient,
     RESTClient,
+    NetworkManager,
     version,
     colors,
     badges,
-    utils: { sleep, formatTime, randomString, escapeHtml }
+    utils: { sleep, formatTime, formatDate, randomString, escapeHtml, truncate }
 };
