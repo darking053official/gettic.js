cec9a5203a8088015018e076808df6de0c29ce1f
Darking053 <darking053@protonmail.com>
2026-04-25 18:22:22 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -6,9 +6,21 @@ function formatTime(date) {
     return new Date(date).toLocaleTimeString('tr-TR', { hour: '2-digit', minute: '2-digit' });
 }
 
+function formatDate(date) {
+    return new Date(date).toLocaleDateString('tr-TR', { day: 'numeric', month: 'long', year: 'numeric' });
+}
+
 function randomString(length = 16) {
     const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
     return Array.from({ length }, () => chars[Math.floor(Math.random() * chars.length)]).join('');
 }
 
-module.exports = { sleep, formatTime, randomString };
+function escapeHtml(str) {
+    return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+function truncate(str, len = 100) {
+    return str.length > len ? str.substring(0, len) + '...' : str;
+}
+
+module.exports = { sleep, formatTime, formatDate, randomString, escapeHtml, truncate };
