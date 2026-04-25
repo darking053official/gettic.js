5203efa9b1dec3c49ce4940987cbd313eeaee9c3
Darking053 <darking053@protonmail.com>
2026-04-25 18:09:34 +0300

    Refactor index.js and update version to 2.0.0
    Updated module exports to include utility functions and increment version to 2.0.0.

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-/**
- * Gettic.js - Resmi Gettic Kütüphanesi
- * github.com/darking053official/gettic.js
- * 
- * Kullanım:
- * const { Client, WebhookClient, RESTClient } = require('gettic.js');
- */
-
-const { Client } = require('./src/client');
-const { WebhookClient } = require('./src/webhook');
-const { RESTClient } = require('./src/rest');
+const { Client } = require('./system');
+const { WebhookClient } = require('./network');
+const { RESTClient } = require('./network');
+const { sleep, formatTime, randomString } = require('./utils');
 
 module.exports = {
     Client,
     WebhookClient,
     RESTClient,
-    version: '1.0.0',
-    
-    createClient: (options) => new Client(options),
-    createWebhook: (url, options) => new WebhookClient(url, options),
-    createREST: (options) => new RESTClient(options)
+    version: '2.0.0',
+    utils: { sleep, formatTime, randomString }
 };
